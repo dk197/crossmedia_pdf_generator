8960656ad413e6889ee2362a2920b7f1154e15c9
liltschou <38752135+liltschou@users.noreply.github.com>
2019-12-10 09:31:40 +0100

    Small.CSS changes

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="row">
-        <p>Startseite</p>
+    <div class="container">
+        <div class="left-bg">
+        </div>
+        <div class="right-bg">
+        </div>
     </div>
 </template>
 
@@ -9,3 +12,69 @@ export default {
     
 }
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+  background: #f1f1f1;
+  margin: 40px auto;
+  width: 80%;
+  height: 360px;
+}
+.left-bg, .right-bg {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 50%;
+  transition: .25s ease;
+}
+.left-bg {
+  background-color:#24800f;
+  left: 0;
+}
+.right-bg {
+  background-color:#5988ad;
+  background-position: right;
+  right: 0;
+}
+.right-bg:before {
+  content: "OR";
+  font-weight: bold;
+  position: absolute;
+  background: white;
+  text-align: center;
+  width: 30px;
+  height: 30px;
+  line-height: 30px;
+  border-radius: 15px;
+  top: 150px;
+  left: 0;
+  -webkit-transform: translate(-50%, -50%);
+}
+.right-bg:after {
+  content: " ";
+  height: 280px;
+  width: 1px;
+  background-color: white;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 20px;
+}
+.right-bg:after {
+  right: 0;
+}
+.left-bg:hover {
+  width: 80%;
+}
+.left-bg:hover + .right-bg {
+  width: 20%;
+}
+.right-bg:hover {
+  width: 80%;
+}
+.right-bg:hover + .left-bg {
+  width: 20%;
+}
+</style>
