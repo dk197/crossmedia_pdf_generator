af9698f91932879fa809af0517fec8bf8cde4d7c
navewi <nathalie.wich@web.de>
2019-12-10 14:49:21 +0100

    css improvements

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,4 +21,25 @@ export default {
 body {
     color: white;
 }
+.nav-link {
+	display: inline-block;
+}
+.container-fluid {
+	margin-top: 50px;
+}
+form > h3 {
+	margin-bottom: 20px;
+}
+.btn {
+	background-color: #42b883;
+	border-color: #42b883;
+}
+.btn:hover {
+	background-color: #42b883;
+	border-color: #42b883;
+	transform: translateY(-2px);
+}
+.nav-link.router-link-exact-active {
+	border-bottom: 2px solid #42b883;
+}
 </style>
