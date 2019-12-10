2c3125adca1b4979b1917730cabbf357b60c50c3
liltschou <38752135+liltschou@users.noreply.github.com>
2019-12-10 14:48:46 +0100

    add background squares

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,17 +1,179 @@
 <template>
-    <div class="container">
+    <div>
+        <div class="area">
+            <div class="container">
         <div class="left-bg">
         </div>
         <div class="right-bg">
         </div>
     </div>
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+        </div>
+        
+    </div>
 </template>
 
 
 <style scoped>
+
+
+*{
+    margin: 0px;
+    padding: 0px;
+}
+
+
+.context {
+    width: 100%;
+    position: absolute;
+    top:50vh;
+    
+}
+
+.context h1{
+    text-align: center;
+    color: #fff;
+    font-size: 50px;
+}
+
+
+.area{
+    background: #343a40;  
+    width: 100%;
+    height:100vh;
+    
+   
+}
+
+.circles{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    
+}
+
+.circles li{
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    animation: animate 25s linear infinite;
+    bottom: -150px;
+    
+}
+
+.circles li:nth-child(1){
+    left: 25%;
+    width: 80px;
+    height: 80px;
+    animation-delay: 0s;
+}
+
+
+.circles li:nth-child(2){
+    left: 10%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 2s;
+    animation-duration: 12s;
+}
+
+.circles li:nth-child(3){
+    left: 70%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 4s;
+}
+
+.circles li:nth-child(4){
+    left: 40%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 0s;
+    animation-duration: 18s;
+}
+
+.circles li:nth-child(5){
+    left: 65%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 0s;
+}
+
+.circles li:nth-child(6){
+    left: 75%;
+    width: 110px;
+    height: 110px;
+    animation-delay: 3s;
+}
+
+.circles li:nth-child(7){
+    left: 35%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 7s;
+}
+
+.circles li:nth-child(8){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+
+.circles li:nth-child(9){
+    left: 20%;
+    width: 15px;
+    height: 15px;
+    animation-delay: 2s;
+    animation-duration: 35s;
+}
+
+.circles li:nth-child(10){
+    left: 85%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 0s;
+    animation-duration: 11s;
+}
+
+
+
+@keyframes animate {
+
+    0%{
+        transform: translateY(0) rotate(0deg);
+        opacity: 1;
+        border-radius: 0;
+    }
+
+    100%{
+        transform: translateY(-1000px) rotate(720deg);
+        opacity: 0;
+        border-radius: 50%;
+    }
+
+}
 .container {
   position: relative;
-  background: #f1f1f1;
+  background-color:#343a40;
   margin: 40px auto;
   width: 80%;
   height: 360px;
@@ -21,7 +183,8 @@
   top: 0;
   bottom: 0;
   width: 77%;
-  transition: .25s ease;
+  transition: .75s ease;
+  z-index: 9999;
 }
 
 .right-bg{
@@ -29,7 +192,8 @@
   top: 0;
   bottom: 0;
   width: 23%;
-  transition: .25s ease;
+  transition: .75s ease;
+  z-index: 9999;
 }
 
 .left-bg {
@@ -56,7 +220,7 @@
   -webkit-transform: translate(-50%, -50%);
 }
 .right-bg:after {
-  content: " ";
+  content: "";
   height: 280px;
   width: 1px;
   background-color: white;
