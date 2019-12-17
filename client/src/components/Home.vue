e56d7ae7265e7ae1e8ed5011e6ffeb8cf3f1f7fd
liltschou <38752135+liltschou@users.noreply.github.com>
2019-12-17 10:17:57 +0100

    integrated textbox in home

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -20,6 +20,86 @@
                     <li></li>
             </ul> -->
         </div>
+        <div class="tabs">
+  <div class="tab-2">
+    <label for="tab2-1">Warum Visitenkarten-Online?</label>
+    <input id="tab2-1" name="tabs-two" type="radio" checked="checked">
+    <div>
+      <h4>Visitenkarten erstellen – die Basis Ihrer Geschäftsausstattung</h4>
+      <p>Ein Geschäftstermin ohne Visitenkartentausch? Unvorstellbar. Visitenkarten sind noch immer eines der wichtigsten Werbemittel. Nutzen Sie Ihre Karte, um sich und Ihr Unternehmen vom ersten Aufeinandertreffen an professionell zu präsentieren. Ein Blick auf Vorder- und Rückseite und in wenigen Sekunden entscheidet sich, welchen Eindruck Ihr Kunde von Ihnen und Ihrer Firma hat. Das Gute: Sie können diesen ersten Eindruck selbst steuern! Entscheidend sind Material und Design. Schließlich muss Ihre Karte – z. B. auf einer Messe oder bei einem geschäftlichen Event – aus einer Vielzahl von Kärtchen hervorstechen und beim Empfänger dauerhaft in Erinnerung bleiben. Von der klassischen Visitenkarte bis hin zur Visitenkarte mit Klapp- oder Aufkleber-Funktion – allesamt erhältlich in verschiedenen Materialen, Papierstärken und mit Veredelungen: Ihre Online-Druckerei bietet Ihnen für die unterschiedlichsten Wünsche eine breite Palette an Möglichkeiten zum Visitenkarten drucken an.</p>
+    </div>
+  </div>
+  <div class="tab-2 t3">
+    <label for="tab2-2">Unsere Preise</label>
+    <input id="tab2-2" name="tabs-two" type="radio">
+    <div style="width: 300%;">
+      <section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+      <!-- Free Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Kostenfrei</h5>
+            <hr>
+            <ul class="fa-ul">
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Visitenkarten Online-Editor</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Download als PDF/X </li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong></strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span></li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Jetzt registrieren</a>
+          </div>
+        </div>
+      </div>
+      <!-- Plus Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Einsteiger</h5>
+            <hr>
+            <ul class="fa-ul">
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Visitenkarten Online-Editor</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Download als PDF/X </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>bestellbar bis 190g/qm</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong></strong></li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Kontaktieren Sie uns hier</a>
+          </div>
+        </div>
+      </div>
+      <!-- Pro Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
+            <hr>
+            <ul class="fa-ul">
+             <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Visitenkarten Online-Editor</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Download als PDF/X </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>bestellbar bis 270g/qm</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>persönliche Beratung</li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Kontaktieren Sie uns hier</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+  </div>
+  <div class="tab-2">
+    <label for="tab2-3">Unser Editor</label>
+    <input id="tab2-3" name="tabs-two" type="radio">
+    <div>
+      <h4>Sofort starten</h4>
+      <p>Verlieren Sie keine Zeit! Bringen Sie noch heute Ihr Geschäft auf eine neue Ebene und werden so zum Erfolgsmensch. Mit unserem Editor erstellen Sie simpel wie professionell Ihren neuen Erfolgsausweis. Geizen Sie nicht mit Ihren USP´s #selflove.</p>
+      <h5>Ihr Visitenkarten-Online Team</h5>
+      <a href="#" class="btn-secondary btn" style="margin-top: 20px">Jetzt zum Editor</a>
+    </div>
+  </div>
+</div>
         
     </div>
 </template>
@@ -28,9 +108,139 @@
 <style scoped>
 
 
-*{
-    margin: 0px;
-    padding: 0px;
+button:focus,
+input:focus,
+textarea:focus,
+select:focus {
+  outline: none; }
+
+.tabs {
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  -webkit-flex-wrap: wrap;
+  -moz-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin: 0 10%;
+  overflow: hidden; }
+  .tabs [class^="tab"] label,
+  .tabs [class*=" tab"] label {
+    color: #efedef;
+    cursor: pointer;
+    display: block;
+    font-size: 1.1em;
+    font-weight: 300;
+    line-height: 1em;
+    padding: 2rem 0;
+    text-align: center; }
+  .tabs [class^="tab"] [type="radio"],
+  .tabs [class*=" tab"] [type="radio"] {
+    border-bottom: 1px solid rgba(239, 237, 239, 0.5);
+    cursor: pointer;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    display: block;
+    width: 100%;
+    -webkit-transition: all 0.3s ease-in-out;
+    -moz-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out; }
+    .tabs [class^="tab"] [type="radio"]:hover, .tabs [class^="tab"] [type="radio"]:focus,
+    .tabs [class*=" tab"] [type="radio"]:hover,
+    .tabs [class*=" tab"] [type="radio"]:focus {
+      border-bottom: 1px solid #fd264f; }
+    .tabs [class^="tab"] [type="radio"]:checked,
+    .tabs [class*=" tab"] [type="radio"]:checked {
+      border-bottom: 2px solid #fd264f; }
+    .tabs [class^="tab"] [type="radio"]:checked + div,
+    .tabs [class*=" tab"] [type="radio"]:checked + div {
+      opacity: 1; }
+    .tabs [class^="tab"] [type="radio"] + div,
+    .tabs [class*=" tab"] [type="radio"] + div {
+      display: block;
+      opacity: 0;
+      padding: 2rem 0;
+      width: 90%;
+      -webkit-transition: all 0.3s ease-in-out;
+      -moz-transition: all 0.3s ease-in-out;
+      -o-transition: all 0.3s ease-in-out;
+      transition: all 0.3s ease-in-out; }
+  .tabs .tab-2 {
+    width: 33%; }
+    .tabs .tab-2 [type="radio"] + div {
+      width: 200%;
+      margin-left: 200%; }
+    .tabs .tab-2 [type="radio"]:checked + div {
+      margin-left: 0; }
+    .tabs .tab-2:last-child [type="radio"] + div {
+      margin-left: 100%; }
+    .tabs .tab-2:last-child [type="radio"]:checked + div {
+      margin-left: -200%; }
+    .tabs .t3 [type="radio"]:checked + div {
+      margin-left: -100%; }
+
+
+ 
+
+.pricing .card {
+  border: none;
+  border-radius: 1rem;
+  transition: all 0.2s;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.pricing hr {
+  margin: 1.5rem 0;
+}
+
+.pricing .card-title {
+  margin: 0.5rem 0;
+  font-size: 0.9rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+}
+
+.pricing .card-price {
+  font-size: 3rem;
+  margin: 0;
+}
+
+.pricing .card-price .period {
+  font-size: 0.8rem;
+}
+
+.pricing ul li {
+  margin-bottom: 1rem;
+}
+
+.pricing .text-muted {
+  opacity: 0.7;
+}
+
+.pricing .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  opacity: 0.7;
+  transition: all 0.2s;
+}
+
+/* Hover Effects on Card */
+
+@media (min-width: 992px) {
+  .pricing .card:hover {
+    margin-top: -.25rem;
+    margin-bottom: .25rem;
+    box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.3);
+  }
+  .pricing .card:hover .btn {
+    opacity: 1;
+  }
 }
 
 
@@ -50,9 +260,7 @@
 
 .area{
     background: #343a40;  
-    width: 100%;
-    height:100vh;
-    
+    width: 100%;   
    
 }
 
