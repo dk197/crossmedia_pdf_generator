163cd54658e81028f540c9a957c25011e7442357
navewi <nathalie.wich@web.de>
2019-12-17 15:22:52 +0100

    added footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="footer">
+        <div class="fotter-left">
+             <a class="logo-link" href="/#/"><img class="logo" src="../assets/logo.svg" ></a>
+        </div>
+        <div class="fotter-right">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .footer {
+        background-color: #42b883;
+        height: 200px;
+    }
+</style>
