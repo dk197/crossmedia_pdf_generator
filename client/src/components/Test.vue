8058f9290016dfc4392090f5cae244efbfa87bc9
liltschou <38752135+liltschou@users.noreply.github.com>
2019-12-17 10:04:38 +0100

    customized tabs

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -2,27 +2,81 @@
    
 <div class="tabs">
   <div class="tab-2">
-    <label for="tab2-1">One</label>
+    <label for="tab2-1">Warum Visitenkarten-Online?</label>
     <input id="tab2-1" name="tabs-two" type="radio" checked="checked">
     <div>
-      <h4>Tab One</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat id velit quis vestibulum. Nam id orci eu urna mollis porttitor. Nunc nisi ante, gravida at velit eu, aliquet sodales dui. Sed laoreet condimentum nisi a egestas.</p><p>Donec interdum ante ut enim consequat, quis varius nulla dapibus. Vivamus mollis fermentum augue a varius. Vestibulum in sapien at lectus gravida lobortis vulputate sed metus. Duis scelerisque justo et maximus efficitur. Donec eu eleifend quam. Curabitur aliquet commodo sapien eget vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum vel aliquet nunc, finibus posuere lorem. Suspendisse consectetur volutpat est ut ornare.</p>
+      <h4>Visitenkarten erstellen – die Basis Ihrer Geschäftsausstattung</h4>
+      <p>Ein Geschäftstermin ohne Visitenkartentausch? Unvorstellbar. Visitenkarten sind noch immer eines der wichtigsten Werbemittel. Nutzen Sie Ihre Karte, um sich und Ihr Unternehmen vom ersten Aufeinandertreffen an professionell zu präsentieren. Ein Blick auf Vorder- und Rückseite und in wenigen Sekunden entscheidet sich, welchen Eindruck Ihr Kunde von Ihnen und Ihrer Firma hat. Das Gute: Sie können diesen ersten Eindruck selbst steuern! Entscheidend sind Material und Design. Schließlich muss Ihre Karte – z. B. auf einer Messe oder bei einem geschäftlichen Event – aus einer Vielzahl von Kärtchen hervorstechen und beim Empfänger dauerhaft in Erinnerung bleiben. Von der klassischen Visitenkarte bis hin zur Visitenkarte mit Klapp- oder Aufkleber-Funktion – allesamt erhältlich in verschiedenen Materialen, Papierstärken und mit Veredelungen: Ihre Online-Druckerei bietet Ihnen für die unterschiedlichsten Wünsche eine breite Palette an Möglichkeiten zum Visitenkarten drucken an.</p>
     </div>
   </div>
   <div class="tab-2 t3">
-    <label for="tab2-2">Two</label>
+    <label for="tab2-2">Unsere Preise</label>
     <input id="tab2-2" name="tabs-two" type="radio">
-    <div>
-      <h4>Tab Two</h4>
-      <p>Quisque sit amet turpis leo. Maecenas sed dolor mi. Pellentesque varius elit in neque ornare commodo ac non tellus. Mauris id iaculis quam. Donec eu felis quam. Morbi tristique lorem eget iaculis consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean at tellus eget risus tempus ultrices. Nam condimentum nisi enim, scelerisque faucibus lectus sodales at.</p>
+    <div style="width: 300%;">
+      <section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+      <!-- Free Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Kostenfrei</h5>
+            <hr>
+            <ul class="fa-ul">
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Visitenkarten Online-Editor</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Download als PDF/X </li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong></strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span></li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Jetzt registrieren</a>
+          </div>
+        </div>
+      </div>
+      <!-- Plus Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Einsteiger</h5>
+            <hr>
+            <ul class="fa-ul">
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Visitenkarten Online-Editor</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Download als PDF/X </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>bestellbar bis 190g/qm</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong></strong></li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Kontaktieren Sie uns hier</a>
+          </div>
+        </div>
+      </div>
+      <!-- Pro Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
+            <hr>
+            <ul class="fa-ul">
+             <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Visitenkarten Online-Editor</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Download als PDF/X </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>bestellbar bis 270g/qm</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>persönliche Beratung</li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Kontaktieren Sie uns hier</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
     </div>
   </div>
   <div class="tab-2">
-    <label for="tab2-3">Three</label>
+    <label for="tab2-3">Unser Editor</label>
     <input id="tab2-3" name="tabs-two" type="radio">
     <div>
-      <h4>Tab Three</h4>
-      <p>Quisque sit amet turpis leo. Maecenas sed dolor mi. Pellentesque varius elit in neque ornare commodo ac non tellus. Mauris id iaculis quam. Donec eu felis quam. Morbi tristique lorem eget iaculis consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean at tellus eget risus tempus ultrices. Nam condimentum nisi enim, scelerisque faucibus lectus sodales at.</p>
+      <h4>Sofort starten</h4>
+      <p>Verlieren Sie keine Zeit! Bringen Sie noch heute Ihr Geschäft auf eine neue Ebene und werden so zum Erfolgsmensch. Mit unserem Editor erstellen Sie simpel wie professionell Ihren neuen Erfolgsausweis. Geizen Sie nicht mit Ihren USP´s #selflove.</p>
+      <h5>Ihr Visitenkarten-Online Team</h5>
+      <a href="#" class="btn-secondary btn" style="margin-top: 20px">Jetzt zum Editor</a>
     </div>
   </div>
 </div>
@@ -108,6 +162,67 @@ select:focus {
       margin-left: -200%; }
     .tabs .t3 [type="radio"]:checked + div {
       margin-left: -100%; }
+
+
+ 
+
+.pricing .card {
+  border: none;
+  border-radius: 1rem;
+  transition: all 0.2s;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.pricing hr {
+  margin: 1.5rem 0;
+}
+
+.pricing .card-title {
+  margin: 0.5rem 0;
+  font-size: 0.9rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+}
+
+.pricing .card-price {
+  font-size: 3rem;
+  margin: 0;
+}
+
+.pricing .card-price .period {
+  font-size: 0.8rem;
+}
+
+.pricing ul li {
+  margin-bottom: 1rem;
+}
+
+.pricing .text-muted {
+  opacity: 0.7;
+}
+
+.pricing .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  opacity: 0.7;
+  transition: all 0.2s;
+}
+
+/* Hover Effects on Card */
+
+@media (min-width: 992px) {
+  .pricing .card:hover {
+    margin-top: -.25rem;
+    margin-bottom: .25rem;
+    box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.3);
+  }
+  .pricing .card:hover .btn {
+    opacity: 1;
+  }
+}
 
 
 </style>
