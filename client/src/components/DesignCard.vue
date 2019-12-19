04795542b67425e788928c1bd828358e967cc4dc
navewi <nathalie.wich@web.de>
2019-12-19 19:40:47 +0100

    css for designcard, code refactor

--- client/src/components/DesignCard.vue
+++ client/src/components/DesignCard.vue
@@ -1,52 +1,67 @@
 <template>
     <div>
-        <h1>Erstelle deine Visitenkarte</h1>
-        <p>Deine Kontaktdaten:</p>
-        <form method="post" action="http://localhost:80/generate/pdf" target="_blank">
-            <h3>Deine Daten:</h3>
-            <div>
-                <select id="attributeSelect" @change="createNewAttribute">
-                    <option disabled :value="currentAttribute">Please select your attribute</option>
-                    <option>Name</option>
-                    <option>Position</option>
-                    <option>Firma</option>
-                    <option>Adresse</option>
-                    <option>Telefon</option>
-                    <option>Fax</option>
-                    <option>E-Mail</option>
-                    <option>Website</option>
-                </select>
-                <select v-model="fontSize" id="fontSize" @change="changeFontSize(currentAttribute)">
-                    <option disabled value="">Please select your font size</option>
-                    <option>xx-small</option>
-                    <option>x-small</option>
-                    <option>small</option>
-                    <option>medium</option>
-                    <option>large</option>
-                    <option>x-large</option>
-                    <option>xx-large</option>
-                </select>
-                <select v-model="fontStyle" id="fontStyle" @change="changeFontStyle(currentAttribute)">
-                    <option disabled value="">Please select your font style</option>
-                    <option>Verdana</option>
-                    <option>Arial</option>
-                    <option>Futura</option>
-                    <option>Times</option>
-                </select>
-                <input type="number" v-model="cardWidth" name="cardWidth"  placeholder="Breite" id="cardWidth" @change="handleWidth">
-                <input type="number" v-model="cardHeight" name="cardHeight" placeholder="Höhe" id="cardHeight" @change="handleHeight">
+        <h1 class="dc-heading" >Erstelle deine Visitenkarte</h1>
+        <div class="dc-content">
+            <div class="dc-input-area">
+                <form method="post" action="http://localhost:80/generate/pdf" target="_blank">
+                    <!-- <h3>Deine Daten:</h3> -->
+                    <div class=dc-options>
+                        <h4>Optionen:</h4>
+                        <p class="dc-label">Schriftgröße:</p>
+                        <select class="dc-input" v-model="fontSize" id="fontSize" @change="changeFontSize(currentAttribute)">
+                            <option disabled value="">Schriftgröße</option>
+                            <option value="8px">8px</option>
+                            <option value="10px">10px</option>
+                            <option value="12px">12px</option>
+                            <option value="14px">14px</option>
+                            <option value="16px">16px</option>
+                            <option value="20px">20px</option>
+                            <option value="25px">25px</option>
+                            <option value="30px">30px</option>
+                        </select>
+                        <p class="dc-label">Schriftart:</p>
+                        <select class="dc-input" v-model="fontStyle" id="fontStyle" @change="changeFontStyle(currentAttribute)">
+                            <option disabled value="">Schriftart</option>
+                            <option>Verdana</option>
+                            <option>Arial</option>
+                            <option>Futura</option>
+                            <option>Times</option>
+                        </select>
+                        <p class="dc-label">Breite:</p><input class="dc-input dc-input-size" type="number" v-model="cardWidth" name="cardWidth"  placeholder="Breite" id="cardWidth" @change="handleWidth">
+                        <p class="dc-label">Höhe:</p><input class="dc-input dc-input-size" type="number" v-model="cardHeight" name="cardHeight" placeholder="Höhe" id="cardHeight" @change="handleHeight">
+                    </div>
+                    <div class="dc-data">
+                        <h4>Daten:</h4>
+                        <select class="dc-input" id="attributeSelect" @change="createNewAttribute">
+                            <option disabled :value="currentAttribute">Datentyp</option>
+                            <option>Name</option>
+                            <option>Position</option>
+                            <option>Firma</option>
+                            <option>Adresse</option>
+                            <option>Telefon</option>
+                            <option>Fax</option>
+                            <option>E-Mail</option>
+                            <option>Website</option>
+                        </select>
+                        <div id="businessCardInput">
+                            <td class="dc-input dc-input-card" v-for="(brick, index) in bricks" :key="index">
+                                <input v-model="bricks[index].data.text" :placeholder="bricks[index].attribute" @focus="changeCurrentAttribute(index)">
+                            </td>
+                        </div>
+                        <div class="dc-submit">                                                    
+                            <input type="hidden" name="htmlInput" id="htmlInput">
+                            <input type="submit" class="btn btn-primary" value="View Pdf" @click="handleHtml">
+                        </div>
+                        
+                    </div>
+                    
+                </form>
             </div>
-            <div id="businessCardInput">
-                <td v-for="(brick, index) in bricks" :key="index">
-                    <input v-model="bricks[index].data.text" :placeholder="bricks[index].attribute" @focus="changeCurrentAttribute(index)">
-                </td>
+            <div class="dc-card-area">
+                <div id="businessCardCanvas" class="parentElement" style="height: 51mm; width: 86mm;">
+                    <text-brick v-for="(brick, index) in bricks" :key="index" :text="brick.data.text" :font-size="brick.data.fontSize" :font-style="brick.data.fontStyle" :id="index"></text-brick>
+                </div>
             </div>
-            <input type="hidden" name="htmlInput" id="htmlInput">
-            <input type="submit" class="btn btn-primary" value="View Pdf" @click="handleHtml">
-        </form>
-        
-        <div id="businessCardCanvas" class="parentElement" style="height: 51mm; width: 86mm;">
-            <text-brick v-for="(brick, index) in bricks" :key="index" :text="brick.data.text" :font-size="brick.data.fontSize" :font-style="brick.data.fontStyle" :id="index"></text-brick>
         </div>
     </div>
 </template>
@@ -71,7 +86,7 @@ export default {
                     data: {
                         fontSize: '',
                         fontStyle: '',
-                        text: 'tests'   
+                        // text: 'tests'   
                     }
                 }
             ]
@@ -134,8 +149,10 @@ export default {
                     data: {
                         fontSize: '',
                         fontStyle: '',
-                        text: 'Bitte eintippen'   
-                    }
+                        // text: 'Bitte eintippen'
+                    },
+                    
+
                 }
                 this.bricks.push(newAttrObj)
             }
@@ -170,12 +187,91 @@ export default {
     }
     #businessCardInput{
         float: left;
+        display: flex;
+        width: 450px;
+        flex-wrap: wrap;
+        margin-top:10px;
     }
     #businessCardCanvas{
         float: left;
-        left: 10%;
+        /* left: 10%; */
         background: white;
         font-size: 12px;
         font-family: Futura;
+    }
+    .dc-heading {
+        margin-left: 10%;
+    }
+    .dc-content {
+        display: flex;
+        width: 100%;
+        position: relative;
+        padding: 0 10%;
+    }
+    .dc-input-area {
+        width: 40%;
+    }
+    .dc-card-area {
+        padding-top: 170px;
+    }
+    .dc-input {
+        display: block;
+        height: calc(1.5em + .75rem + 2px);
+        padding: .375rem .75rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #495057;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #ced4da;
+        border-radius: .25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+    .dc-input-card {
+        width: 200px;
+        margin-right: 10px;
+        margin-bottom: 10px;
+    }
+    .dc-input input{
+        border: none;
+        width: 180px;
+    }
+    .dc-data, .dc-options, .dc-submit {
+        width:  100%
+    }
+    .dc-options {
+        display: flex;
+        margin-bottom: 50px;
+    }
+    .dc-options h4{
+        margin-right: 20px;
+    }
+    .dc-options > * {
+        margin-right: 10px
+    }
+    .dc-data select{
+        width: 200px;
+    }
+    .dc-input-size {
+        width: 50px;
+    }
+    .dc-card-options {
+        display: flex;
+        margin-bottom: 30px;
+    }
+    .dc-card-options input {
+        width: 100px;
+    }
+    .dc-data {
+        display: flex;
+        flex-direction: column;
+    }
+    .dc-submit {
+        margin-top: 50px;
+    }
+    .dc-label{
+        padding-top: 7px;
+
     }
 </style>
