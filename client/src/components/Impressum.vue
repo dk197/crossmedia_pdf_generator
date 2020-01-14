46e5e5feddda05c50abaa060680827b7880f17ff
navewi <nathalie.wich@web.de>
2020-01-14 12:04:52 +0100

    footer und impressum component

---
+++ client/src/components/Impressum.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Impressum</h1>
+        <p>Dies ist eine Studienarbeit von:</p>
+        <p>Jonas Hauser, Johannes Manner, Jakob Schlagenhaufer, Dominik Schäfer und Nathalie Wich <br> 
+        aus dem Studiengang ON17 an der DHBW Mosbach</p>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        alert('test')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
