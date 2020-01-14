4cd3e7cf6294a0a13c71e43dc89f9c592dde1ac6
Jakob <jakob.schlagenhaufer@gmail.com>
2020-01-14 11:28:43 +0100

    test qr code on card

--- api/app/Http/Controllers/PdfController.php
+++ api/app/Http/Controllers/PdfController.php
@@ -35,6 +35,7 @@ class PdfController extends Controller
             <div data-v-4a7fb514="" class="innerElement">
                 tests
             </div>
+            <img src="https://qrickit.com/api/qr.php?d=BEGIN%3aVCARD%0d%0aVERSION%3a3.0%0d%0aN%3aDoe%3bJohn%0d%0aORG%3aABC+Co.%0d%0aTITLE%3aPresident%0d%0aEMAIL%3ajohndoe%40email.com%0d%0aTEL%3bTYPE%3dCELL%3a5555555555%0d%0aTEL%3bTYPE%3dWORK%2cVOICE%3a5551231234%0d%0aNOTE%3aTest%0d%0aURL%3awww.abc.com%0d%0aADR%3a%3b%3bSuite+451+Main+St.%253bMiami%253bFlorida%253b123456%253bUSA%0d%0aEND%3aVCARD%0A&addtext=&txtcolor=000000&fgdcolor=000000&bgdcolor=FFFFFF&qrsize=200" alt="Smiley face" height="42" width="42">
         </div>
         <style>
         .drag-it-dude {
