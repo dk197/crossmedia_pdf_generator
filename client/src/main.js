b5a166c01a51e677a7afcf3567ce796a4ae57a17
liltschou <38752135+liltschou@users.noreply.github.com>
2020-01-21 13:35:30 +0100

    Revert "Revert "color picker cmyk""
    This reverts commit d19f67b93d434be2fd1880a233249ea7c02c795d.

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,13 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
 import { store } from './store'
+import VueChannelColorPicker from 'vue-channel-color-picker';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
+Vue.use(VueChannelColorPicker);
 
 const router = new VueRouter({
     routes
