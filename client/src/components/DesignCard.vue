1d956d6032a2691b7e2b94f386d8aec08d8a8ab3
dk197 <38752159+dk197@users.noreply.github.com>
2020-01-28 08:38:46 +0100

    add names for designcard inputs

--- client/src/components/DesignCard.vue
+++ client/src/components/DesignCard.vue
@@ -78,7 +78,7 @@
                         <h4>Daten:</h4>
                         <div id="businessCardInput">
                             <td class="dc-input dc-input-card" v-for="(brick, index) in bricks" :key="index">
-                                <input v-model="bricks[index].data.text" :placeholder="bricks[index].attribute" @focus="changeCurrentAttribute(index)">
+                                <input v-model="bricks[index].data.text" :name="bricks[index].attribute" :placeholder="bricks[index].attribute" @focus="changeCurrentAttribute(index)">
                             </td>
                             <input  class="dc-button btn btn-primary" type="button" value=" +/- QR-Code" @click="toggleQr()">
                             <input  class="dc-button btn btn-primary" type="button" value=" +/- Logo " @click="toggleLogo()">
